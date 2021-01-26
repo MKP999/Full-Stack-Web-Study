71e32034eeb6654eaddcc4abf2df3f9aaa1c7c27
mkp <993646298@qq.com>
2021-01-26 17:55:22 +0800

    实现vue.extend创建弹窗 安装router vuex

--- vue-first-part/src/main.js
+++ vue-first-part/src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Notice from '@/components/Notice.vue'
+import create from '@/util/create'
+import store from './store'
+
+Vue.prototype.$notice = function (props) {
+  create(Notice,props).show()
+}
 
 Vue.config.productionTip = false
 
@@ -9,5 +16,6 @@ Vue.prototype.$Bus = new Vue()
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
